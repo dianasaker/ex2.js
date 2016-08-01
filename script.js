7fe54470c56a5e7027cf2e3abc3b2c149f063e79
Diana Saker <Diana Saker>
2016-08-01 17:14:44 +0300

    ex2.js

---
+++ script.js
@@ -0,0 +1,13 @@
+var me={};
+me={
+	name:"Diana",
+	height:"170",
+	eyescolor:"blue",
+	Location:{"isreal?":"yes",
+	           country:"Isifya",
+     }
+	
+};
+me.eyescolor="green";
+me.language={english:"yes",arabic:"yes",hebrew:"yes",};
+typeof(me.Languages.hebrew);
